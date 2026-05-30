3f535be5e46c4675fa5fa05a122f92b8816eab63
AbdelrhmanKhaled76 <bodi.khaled24@gmail.com>
2026-05-30 15:11:39 +0300

    feat : routing added

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,25 @@
 // import { StrictMode } from 'react'
+import Home from './pages/home/home.js';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />, // App is the main layout
+    children: [
+      {
+        path: "/",
+        element: <Home />, // Renders when the URL is exactly "/"
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
-    <App />
+  <RouterProvider router={router} />
   // </StrictMode>,
 )
